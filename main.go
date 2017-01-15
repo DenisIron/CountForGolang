f7ef32cabac6830cb69ef93b53d121f95809fc69
Denis Karandeev <den.strongman@gmail.com>
2017-01-15 16:21:52 +0800

    added slice arrayUrls, func urls

--- main.go
+++ main.go
@@ -4,48 +4,60 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 	"sync"
 )
 
 func main() {
 	var (
-		totalResult = 0
-		k           = 5
+		totalResult = 0 //Итоговое кол-во вхождений
+		k           = 5 //Макс. число горутин
 		gorout      = 0
 		wait        sync.WaitGroup
 	)
-	searchGo := "Go"             //Прописываем какие вхождения будем искать на сайтах
-	urls := make([]string, 1000) //Создаем slice, заполняемый вводимыми сайтами
-	for i := range urls {
-		fmt.Scanln(&urls[i]) //Вводим URL, в котором будем искать количество вхождений
-		url := urls[i]
+	var arrayUrls []string //Создаем slice, заполняемый вводимыми сайтами
+	urls(arrayUrls)        //Заполняем slice сайтами
+
+	for i := range arrayUrls {
+		url := arrayUrls[i]
+		if arrayUrls[i] == "" {
+			break
+		}
 		if gorout < k {
 			wait.Add(1)
-			go countGo(url, &totalResult, &gorout, searchGo) //Запускаем горутины для 5 сайтов
+			go countGo(url, &totalResult, &gorout) //Запускаем горутины для 5 сайтов
 		} else {
 			wait.Wait() //Если горутин больше 5, то ждем пока хотя бы одна из них не выполнится
 			wait.Add(1)
-			go countGo(url, &totalResult, &gorout, searchGo)
+			go countGo(url, &totalResult, &gorout)
 		}
-		if urls[i] == "" {
-			break
-		}
+
 	}
+
 	fmt.Printf("Total: %d\n", totalResult)
 }
 
-func countGo(url string, totalResult *int, gorout *int, searchGo string) {
+func countGo(url string, totalResult *int, gorout *int) {
 	*gorout++
 	resp, err := http.Get(url)
 	site, err := ioutil.ReadAll(resp.Body)
 	er(err)
-	count := strings.Count(string(site), searchGo) //Считает количество вхождений на сайте
+	count := strings.Count(string(site), "Go") //Считает количество вхождений на сайте
 	fmt.Printf("Count for %s = %d\n", url, count)
 	*totalResult += count //Суммируем вхождения на всех заданных сайтах
 	*gorout--
 }
 
+func urls(urls []string) {
+	//scanner := bufio.NewScanner(os.Stdin)    //Считываем строку с сайтами из командной строки
+	urlsStdin, err := ioutil.ReadAll(os.Stdin) //Проверяем содержимое командной строки
+	if err != nil {
+		urls = strings.Split(string(urlsStdin), "\n")
+	} else {
+		er(err)
+	}
+}
 func er(err error) {
 	if err != nil {
 		panic(er)
