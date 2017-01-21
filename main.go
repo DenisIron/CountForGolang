dd9bdf1fc90a8d2f36c5e328f0f09be87f20ea37
Denis Karandeev <den.strongman@gmail.com>
2017-01-21 11:14:24 +0800

    added chan with buffer, delete var gorout

--- main.go
+++ main.go
@@ -12,21 +12,21 @@ import (
 
 func main() {
 	var (
+		wait        sync.WaitGroup
 		totalResult = 0 //Итоговое кол-во вхождений
 		k           = 5 //Макс. число горутин
-		//gorout      = 0
-		wait sync.WaitGroup
 	)
 	finalAllUrls := make(chan bool) // канал для сигнала о том, что все горутины выполнены
 	finalOneUr := make(chan bool)   // канал для сигнала о выполнении 1 горутины
-	chUr := make(chan string)       // канал для сайтов
+	chUr := make(chan string, k)    // буферизированный канал для сайтов
 
-	urls(chUr) //Заполняем канал сайтами
+	go urls(chUr) //Заполняем канал сайтами
 
-	for gorout := 0; gorout < k; gorout++ { // создаем замыкание, тем самым ограничив кол-во горутин
+	for range chUr {
 		go countGo(&totalResult, finalOneUr, chUr, &wait) //Запускаем горутины для 5 сайтов
-		//<-finalOneUr                                      //Если горутин больше 5, то ждем пока хотя бы одна из них выполнится
 	}
+	//<-finalOneUr                                      //Если горутин больше 5, то ждем пока хотя бы одна из них выполнится
+
 	go allUrls(finalAllUrls, &wait) //Функция для проверки выполнения всех горутин
 	<-finalAllUrls                  // Подаем сигнал о том, что все горутины выполнены
 	fmt.Printf("Total: %d\n", totalResult)
@@ -34,7 +34,7 @@ func main() {
 
 func allUrls(finalAllUrls chan bool, wait *sync.WaitGroup) {
 	wait.Wait() // Ждем завершения всех горутин
-	finalAllUrls <- true
+	//finalAllUrls <- true
 }
 func countGo(totalResult *int, oneUr chan bool, chUr chan string, wait *sync.WaitGroup) {
 	wait.Add(1)
@@ -45,7 +45,7 @@ func countGo(totalResult *int, oneUr chan bool, chUr chan string, wait *sync.Wai
 	er(err)
 	count := strings.Count(string(site), "Go") //Считает количество вхождений на сайте
 	fmt.Printf("Count for %s = %d\n", url, count)
-	*totalResult += count //Суммируем вхождения на всех заданных сайтах
+	*totalResult += count //Суммирует вхождения на всех заданных сайтах
 	wait.Add(-1)
 	oneUr <- true
 }
